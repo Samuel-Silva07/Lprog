9cf8978de54812eeb1431f46c41dda912f30ec71
Samuel Silva <149081302+Samuel-Silva07@users.noreply.github.com>
2024-07-29 12:53:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Aula1 {
+  double f(double x,
+          double a,
+          double b,
+          double c){ 
+    return y = a*Math.Pow(x,2) + b*x + c;
+    return y;
+  }
+public static void Main (string[] args)  {
+  int v = 0;
+  Console.writeline (v);
+  double y = f(1,2,3,4);
+  Console.WriteLine(y);
+  double  x = double.Parse(
+    Console.ReadLine ()
+    );
+    y = false(x,2,3,4);
+  Console.WriteLine (y);  
+  }
+}
